8e80b99f8b6ca9a5f34e881f5b03c9864f07b3ed
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2022-10-11 11:04:37 +0200

    add solution

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,17 @@
 public class Exercise {
 
   public static void main(String[] args) {
-    // implement exercise here
+		String name = "Daniel";
+		int age = 40;
+		char gender = 'M';
+
+		Exercise.printPersonData("Daniel", 40, 'M');
+		Exercise.printPersonData(name, age, gender);
   }
+
+	public static void printPersonData(String name, int age, char gender) {
+    System.out.println("Name: " + name);
+		System.out.println("Alter: " + age);
+		System.out.println("Geschlecht: " + gender);
+	}
 }
