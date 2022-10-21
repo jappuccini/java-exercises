7729799e713f31637850fb9f02322a4cfca231ce
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2022-10-21 17:25:01 +0200

    implement solution

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,37 @@
+import java.util.Scanner;
+
 public class Exercise {
 
   public static void main(String[] args) {
-    // implement exercise here
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Gib bitte den Namen eingeben: ");
+		String name = sc.next();
+
+		System.out.print("Gib bitte das Geschlecht eingeben (m, w, d): ");
+		char gender = sc.next().charAt(0);
+
+		String title;
+		switch (gender) {
+		case 'm':
+			title = "Herr";
+			break;
+		case 'w':
+			title = "Frau";
+			break;
+		default:
+			title = "";
+			break;
+		}
+
+		// String title = switch (gender) {
+		// case 'm' -> "Herr";
+		// case 'w' -> "Frau";
+		// default -> "";
+		// };
+
+		System.out.println("Hallo " + title + " " + name);
+
   }
 }
