ab77268960b4559e1287d2b1291ff966c6bb8f1b
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2022-10-21 16:15:10 +0200

    implement solution

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Exercise {
 
   public static void main(String[] args) {
-    // implement exercise here
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Gib bitte eine ganze Zahl ein: ");
+		int a = sc.nextInt();
+
+		System.out.print("Gib bitte eine weitere ganze Zahl ein: ");
+		int b = sc.nextInt();
+
+		double c = (a * 100.0) / b;
+
+		System.out.println("Ergebnis: " + a + " von " + b + " sind " + c + "%");
   }
 }
