76c8654b9eee44ee7cb41a1a26a2f4bc0140ee44
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2022-10-21 16:02:10 +0200

    implement solution

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class Exercise {
 
   public static void main(String[] args) {
-    // implement exercise here
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Gib bitte eine ganze Zahl ein: ");
+		int a = sc.nextInt();
+
+		System.out.print("Gib bitte eine weitere ganze Zahl ein: ");
+		int b = sc.nextInt();
+
+		int c;
+
+		c = a + b;
+
+		System.out.println("Ergebnis: " + a + " + " + b + " = " + c);
   }
 }
