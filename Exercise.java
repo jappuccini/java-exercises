ba16919715030c9c4496756c02bbf7faff1cf492
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2022-10-21 16:57:49 +0200

    implement solution

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class Exercise {
 
   public static void main(String[] args) {
-    // implement exercise here
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Gib bitte eine ganze Zahl ein: ");
+		int a = sc.nextInt();
+
+		int b = a % 2;
+
+		if (b == 0) {
+			System.out.println("Ergebnis: Die eingegebene Zahl ist gerade");
+		} else {
+			System.out.println("Ergebnis: Die eingegebene Zahl ist ungerade");
+		}
+
   }
 }
