c212b2687760ae9568db12a82a2268294a5bef31
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2022-10-21 08:47:59 +0200

    add solution

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,14 @@
 public class Exercise {
 
   public static void main(String[] args) {
-    // implement exercise here
+    int a = 13;
+    int b = 4;
+    int c = a / b;
+    int d = a % b;
+
+		System.out.println("a: " + a);
+		System.out.println("b: " + b);
+		System.out.println("Ganzzahliger Quotient von a/b: " + c);
+		System.out.println("Divisionsrest von a/b: " + d);
   }
 }
