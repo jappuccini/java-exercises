21f2ced7d43d9f6c1c450ba63406920f9f5e8ec3
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2022-10-24 09:45:27 +0200

    implement soluton

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
+
 public class Exercise {
 
   public static void main(String[] args) {
-    // implement exercise here
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Gib bitte eine Zeichenkette ein: ");
+		String text = sc.nextLine();
+
+		System.out.print("Gib bitte das zu analysierende Zeichen ein: ");
+		char token = sc.next().charAt(0);
+
+		int count = 0;
+		for (int i = 0; i < text.length(); i++) {
+			if (text.charAt(i) == token) {
+				count++;
+			}
+		}
+
+		double percentage = count * 100 / text.length();
+
+		System.out.printf("Absoluter Anteil: %d%n", count);
+		System.out.printf("Prozentualer Anteil: %.2f%%%n", percentage);
   }
 }
