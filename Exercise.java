3cbc8d0fc2c871da39a1451b136b06f82ef74038
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2022-10-24 15:09:23 +0200

    implement solution

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,29 @@
 public class Exercise {
 
   public static void main(String[] args) {
-    // implement exercise here
+		int[] numbers = { 4, 8, 2, 6, 9, 1 };
+
+		System.out.print("Eingabe: ");
+		print(numbers);
+
+		reverse(numbers);
+
+		System.out.print("Ausgabe: ");
+		print(numbers);
   }
+
+	public static void print(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+		System.out.println();
+	}
+
+	public static void reverse(int[] array) {
+		for (int i = 0; i < array.length / 2; i++) {
+			int tmp = array[array.length - 1 - i];
+			array[array.length - 1 - i] = array[i];
+			array[i] = tmp;
+		}
+	}
 }
