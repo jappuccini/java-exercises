426fd64deb18e594e5073d0de0414fcf099bd317
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2022-10-24 11:42:08 +0200

    implement solution

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,39 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Exercise {
 
   public static void main(String[] args) {
-    // implement exercise here
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+
+		Random rnd = new Random();
+		int randomNumber = rnd.nextInt(100) + 1;
+
+		int attempts = 0;
+
+		boolean loop;
+		do {
+			attempts++;
+
+			System.out.print("Gib bitte Deinen Tipp ein: ");
+			int tip = sc.nextInt();
+
+			if (tip == randomNumber) {
+				System.out.println("Richtig. Du hast " + attempts + " Versuche benoetigt");
+				break;
+				// return
+			} else if (tip > randomNumber) {
+				System.out.println("Leider falsch, die gesuchte Zahl ist kleiner");
+			} else {
+				System.out.println("Leider falsch, die gesuchte Zahl ist groesser");
+			}
+
+			System.out.print("Moechtest Du nochmals raten (true, false)?: ");
+			loop = sc.nextBoolean();
+		} while (loop);
+
+		System.out.println("Die gesuchte Zahl war " + randomNumber);
+
   }
 }
