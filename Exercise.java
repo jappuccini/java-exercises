f27deb2edd075df43338339451535c7d54866c9f
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2022-10-24 11:46:28 +0200

    implement solution

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
+
 public class Exercise {
 
   public static void main(String[] args) {
-    // implement exercise here
+
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
+
+		int k;
+		double p;
+
+		boolean loop;
+		do {
+			System.out.print("Gib bitte das Startkapital ein (in Euro): ");
+			k = scanner.nextInt();
+
+			System.out.print("Gib bitte den Prozentsatz ein: ");
+			p = scanner.nextDouble();
+
+			System.out.println("Ergebnis: Der Jahreszins betraegt " + (int) calculateInterestPerAnnum(k, p) + " Euro");
+
+			System.out.print("Willst Du einen weiteren Jahreszins berechnen (true, false)?: ");
+			loop = scanner.nextBoolean();
+		} while (loop);
+
   }
+
+	static double calculateInterestPerAnnum(int k, double p) {
+		return k * p / 100;
+	}
 }
