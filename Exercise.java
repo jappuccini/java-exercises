d83f60452ea7a7703a28bb89a08dda64909cc7ed
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2022-10-24 09:39:04 +0200

    implement solution

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,26 @@
 public class Exercise {
 
   public static void main(String[] args) {
-    // implement exercise here
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Gib bitte eine ganze Zahl ein: ");
+		int number = scanner.nextInt();
+
+		boolean isPrime = true;
+
+		if (number < 2) {
+			isPrime = false;
+		} else if (number > 2) {
+			for (int i = 2; i < number; i++) {
+				if (number % i == 0) {
+					isPrime = false;
+					break;
+				}
+			}
+		}
+
+		System.out.println(isPrime ? "Ergebnis: Die eingegebene Zahl ist eine Primzahl"
+				: "Ergebnis: Die eingegebene Zahl ist keine Primzahl");
   }
 }
