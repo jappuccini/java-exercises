38e4350d506676ed2e5a1f5c1a04ca877dce5987
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2022-10-26 11:49:51 +0200

    implement solution

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,21 @@
+import java.util.ArrayList;
+
 public class Exercise {
 
   public static void main(String[] args) {
-    // implement exercise here
+    ArrayList<Dice> dices = new ArrayList<>();
+
+    for (int i = 1; i <= 5; i++) {
+      Dice dice = new Dice(i);
+      dices.add(dice);
+    }
+
+    DiceCup diceCup = new DiceCup();
+
+    System.out.println("ID - Wuerfelwert");
+    for (int i = 1; i <= 5; i++) {
+      System.out.println("Wurf " + i);
+      diceCup.rollTheDices(dices);
+    }
   }
 }
