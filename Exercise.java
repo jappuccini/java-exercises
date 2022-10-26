160e6f35ce3b48b94fbfff403d52cf00ce1dfa25
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2022-10-26 14:59:20 +0200

    implement solution

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Exercise {
 
   public static void main(String[] args) {
-    // implement exercise here
+    @SuppressWarnings("resource")
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Gib bitte einen Wert zwischen -1 und -32.768 ein: ");
+    short s = sc.nextShort();
+
+    System.out.println("Ergebnis: Der vorzeichenfreie Dezimalwert betraegt " + Short.toUnsignedInt(s));
   }
 }
