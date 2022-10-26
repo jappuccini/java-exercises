8da7b9c3869b3cc4ac905a40a66630abd651c699
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2022-10-26 10:22:38 +0200

    implement solution

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,16 @@
 public class Exercise {
 
   public static void main(String[] args) {
-    // implement exercise here
+
+		Vehicle vehicle = new Vehicle();
+
+		vehicle.setMake("Porsche");
+		vehicle.setModel("911");
+
+		vehicle.accelerate(30);
+		vehicle.accelerate(30);
+		vehicle.brake(20);
+		vehicle.accelerate(40);
+
   }
 }
