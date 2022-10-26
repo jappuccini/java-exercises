8d4dcafa13c390d41fac89b498f3d2456d8db595
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2022-10-26 14:36:58 +0200

    implement solution

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,8 @@
 public class Exercise {
 
   public static void main(String[] args) {
-    // implement exercise here
+    for (double x = 0; x < 1; x += 0.1) {
+      System.out.printf("x = %.1f, sin(x) = %.1f%n", x, Math.sin(x));
+    }
   }
 }
