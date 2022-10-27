4ba6d62bf86ed7afa869b9f5ffda3d474800d6d0
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2022-10-27 14:17:38 +0200

    implement solution

--- Exercise.java
+++ Exercise.java
@@ -1,24 +1,16 @@
 public class Exercise {
 
   public static void main(String[] args) {
-    int numberOfVehicles;
-    numberOfVehicles = Vehicle.getNumberOfVehicles();
-    System.out.println("Anzahl Fahrzeuge: " + numberOfVehicles);
+    Rental rental = new Rental("Fahrzeugvermietung Mueller");
 
     Car car1 = new Car("Porsche", "911", Engine.ELECTRO, 2);
     Truck truck1 = new Truck("MAN", "TGX", Engine.DIESEL, 20);
     Car car2 = new Car("Opel", "Zafira Life", Engine.DIESEL, 7);
 
-    numberOfVehicles = Vehicle.getNumberOfVehicles();
-    System.out.println("Anzahl Fahrzeuge: " + numberOfVehicles);
+    rental.addVehicle(car1);
+    rental.addVehicle(truck1);
+    rental.addVehicle(car2);
 
-    car1.print();
-    truck1.print();
-    car2.print();
-
-    car1.accelerate(50);
-    truck1.transform();
-    car1.doATurboBoost();
-    truck1.transform();
+    rental.print();
   }
 }
