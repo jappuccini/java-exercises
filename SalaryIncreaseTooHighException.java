70b9ff171feb2e02ae04d5f5161e9a3873bb3fc2
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2022-10-27 17:48:50 +0200

    implement solution

---
+++ SalaryIncreaseTooHighException.java
@@ -0,0 +1,8 @@
+public class SalaryIncreaseTooHighException extends Exception {
+
+  private static final long serialVersionUID = 1L;
+
+  public SalaryIncreaseTooHighException() {
+    super("Das neue Gehalt darf maximal 10% ueber dem bisherigen Gehalt liegen");
+  }
+}
