173f96dd1e64a8541f6d647f705a824a68c87944
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2022-10-27 17:23:12 +0200

    implement solution

---
+++ InvalidValueException.java
@@ -0,0 +1,9 @@
+public class InvalidValueException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public InvalidValueException() {
+		super("Ungueltiger Wert: der eingehende Wert muss groesser als Null sein");
+	}
+
+}
