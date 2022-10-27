f8937b47b5d40d6275c318777483c48968822a46
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2022-10-27 14:07:11 +0200

    implement solution

--- Exercise.java
+++ Exercise.java
@@ -3,10 +3,19 @@ public class Exercise {
   public static void main(String[] args) {
     System.out.println("Anzahl Fahrzeuge: " + Vehicle.getNumberOfVehicles());
 
-    Vehicle vehicle1 = new Vehicle("Porsche", "911", Engine.ELECTRO);
-    Vehicle vehicle2 = new Vehicle("MAN", "TGX", Engine.DIESEL);
-    Vehicle vehicle3 = new Vehicle("Opel", "Zafira Life", Engine.DIESEL);
+    Car car1 = new Car("Porsche", "911", Engine.ELECTRO, 2);
+    Truck truck1 = new Truck("MAN", "TGX", Engine.DIESEL, 20);
+    Car car2 = new Car("Opel", "Zafira Life", Engine.DIESEL, 7);
 
     System.out.println("Anzahl Fahrzeuge: " + Vehicle.getNumberOfVehicles());
+
+    System.out.println(car1.toString());
+    System.out.println(truck1.toString());
+    System.out.println(car2.toString());
+
+    car1.accelerate(50);
+    truck1.transform();
+    car1.doATurboBoost();
+    truck1.transform();
   }
 }
