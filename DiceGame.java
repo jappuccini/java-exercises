763401a5f40c0843be224206d13bae4641cc9b8a
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2022-10-27 14:47:22 +0200

    implement polymorphy

--- DiceGame.java
+++ DiceGame.java
@@ -43,31 +43,56 @@ public class DiceGame {
   }
 
   private void move(Player player) {
-    dices.add(new Dice(1));
-    dices.add(new Dice(2));
-    dices.add(new Dice(3));
-    System.out.println(player.getName() + " hat aktuell " + player.getScore() + " Punkte");
-    System.out.print(player.getName() + ", moechtest Du wuerfeln (true, false)?: ");
-    if (scanner.nextBoolean()) {
-      diceCup.rollTheDices(dices);
-      int score = 0;
-      for (Dice dice : dices) {
-        score += dice.getValue();
-      }
-      System.out.println(player.getName() + " hat " + score + " Punkte");
-      player.setScore(player.getScore() + score);
-      System.out.println(player.getName() + " hat insgesamt " + player.getScore() + " Punkte");
-      if (player.getScore() > 50) {
-        System.out.println(player.getName() + " hat verloren");
-        player.setScore(0);
-        player.setActive(false);
-        numberOfActivePlayers--;
-      }
-    } else {
-      player.setActive(false);
-      numberOfActivePlayers--;
-    }
-    System.out.println();
-    dices.clear();
+		int input;
+		System.out.println(player.getName() + " hat aktuell " + player.getScore() + " Punkte");
+		if (player.getSpecialAvailable()) {
+			System.out.print(player.getName() + ", moechtest Du einmalig Spezialwuerfel verwenden (1=ja, 2=nein)?: ");
+			input = scanner.nextInt();
+			if (input == 1) {
+				System.out.print(player.getName()
+						+ ", welche Spezialwuerfel moechtest Du verwenden (1=4-5-6-Wuerfel, 2=1-2-3-Wuerfel)?: ");
+				input = scanner.nextInt();
+				if (input == 1) {
+					dices.add(new HighValueDice(4));
+					dices.add(new HighValueDice(5));
+					dices.add(new HighValueDice(6));
+				} else {
+					dices.add(new LowValueDice(7));
+					dices.add(new LowValueDice(8));
+					dices.add(new LowValueDice(9));
+				}
+				player.setSpecialAvailable(false);
+			} else {
+				dices.add(new Dice(1));
+				dices.add(new Dice(2));
+				dices.add(new Dice(3));
+			}
+		} else {
+			dices.add(new Dice(1));
+			dices.add(new Dice(2));
+			dices.add(new Dice(3));
+		}
+		System.out.print(player.getName() + ", moechtest Du wuerfeln (true, false)?: ");
+		if (scanner.nextBoolean()) {
+			diceCup.rollTheDices(dices);
+			int score = 0;
+			for (Dice dice : dices) {
+				score += dice.getValue();
+			}
+			System.out.println(player.getName() + " hat " + score + " Punkte");
+			player.setScore(player.getScore() + score);
+			System.out.println(player.getName() + " hat insgesamt " + player.getScore() + " Punkte");
+			if (player.getScore() > 50) {
+				System.out.println(player.getName() + " hat verloren");
+				player.setScore(0);
+				player.setActive(false);
+				numberOfActivePlayers--;
+			}
+		} else {
+			player.setActive(false);
+			numberOfActivePlayers--;
+		}
+		System.out.println();
+		dices.clear();
   }
 }
