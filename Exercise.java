c78ed89a2516f5216a7e12b22694f8965bfeabbe
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2022-10-27 15:29:43 +0200

    implement solution

--- Exercise.java
+++ Exercise.java
@@ -1,7 +1,9 @@
 public class Exercise {
 
   public static void main(String[] args) {
+    TravelAgency travelAgency = new TravelAgency("Reisebuero Schmidt");
     Rental rental = new Rental("Fahrzeugvermietung Mueller");
+    travelAgency.addPartner(rental);
 
     Car car1 = new Car("Porsche", "911", Engine.ELECTRO, 2);
     Truck truck1 = new Truck("MAN", "TGX", Engine.DIESEL, 20);
@@ -11,6 +13,6 @@ public class Exercise {
     rental.addVehicle(truck1);
     rental.addVehicle(car2);
 
-    rental.print();
+    travelAgency.print();
   }
 }
