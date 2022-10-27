53ea99a9645a4b35aad5118bba2b447f911f82ef
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2022-10-27 17:31:29 +0200

    implement solution

---
+++ BarrelOverflowException.java
@@ -0,0 +1,8 @@
+public class BarrelOverflowException extends Exception {
+
+  private static final long serialVersionUID = 1L;
+
+  public BarrelOverflowException() {
+    super("Das war der Tropfen, der das Fass zum Ueberlaufen gebracht hat");
+  }
+}
