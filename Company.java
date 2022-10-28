6aab0ef3e99a9650391416172cb4e573a8e774ff
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2022-10-28 14:51:03 +0200

    implement solution

--- Company.java
+++ Company.java
@@ -22,4 +22,46 @@ public class Company {
       e.print();
     }
   }
+
+  public class Employee {
+
+    private int employeeId;
+    private Person person;
+    private int salary;
+
+    public Employee(int employeeId, Person person, int salary) {
+      this.employeeId = employeeId;
+      this.person = person;
+      this.salary = salary;
+    }
+
+    public int getEmployeeId() {
+      return employeeId;
+    }
+
+    public String getName() {
+      return person.getName();
+    }
+
+    public int getSalary() {
+      return salary;
+    }
+
+    public void setSalary(int salary) throws SalaryDecreaseException, SalaryIncreaseTooHighException {
+      if (salary < this.salary) {
+        throw new SalaryDecreaseException();
+      }
+
+      double increase = (double) (salary - this.salary) / this.salary;
+      if (increase > 0.1) {
+        throw new SalaryIncreaseTooHighException();
+      }
+
+      this.salary = salary;
+    }
+
+    public void print() {
+      System.out.println(employeeId + " - " + getName() + " - " + salary + " Euro");
+    }
+  }
 }
