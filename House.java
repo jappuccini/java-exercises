7c9070def0a05e0510182e31b68a1716e66e66fe
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2022-10-28 14:34:14 +0200

    implement solution

---
+++ House.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class House {
+
+  private ArrayList<Room> rooms;
+
+  public House() {
+    rooms = new ArrayList<>();
+  }
+
+  public void addRoom(Room room) {
+    rooms.add(room);
+  }
+
+  public void displayRooms() {
+    for (Room r : rooms) {
+      r.displayRoom();
+    }
+  }
+
+  public class Room {
+
+    private String name;
+
+
+    public Room(String name) {
+      this.name = name;
+    }
+
+    public void displayRoom() {
+      System.out.println(name);
+    }
+  }
+}
