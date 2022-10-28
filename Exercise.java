2283be4aa22c595c1e237ea0c3c2f4f0a35ff4db
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2022-10-28 15:23:24 +0200

    implement solution

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,12 @@
 public class Exercise {
 
   public static void main(String[] args) {
-    // implement exercise here
+    TelephoneBook telephoneBook = new TelephoneBook();
+
+    telephoneBook.addEntry(new TelephoneBook.Person("Hans Maier"), new TelephoneBook.TelephoneNumber("+49 67234 887261"));
+    telephoneBook.addEntry(new TelephoneBook.Person("Peter Mueller"), new TelephoneBook.TelephoneNumber("+49 7529 87612"));
+    telephoneBook.addEntry(new TelephoneBook.Person("Lisa Schmidt"), new TelephoneBook.TelephoneNumber("+49 8762 52081"));
+
+    System.out.println(telephoneBook.getTelephoneNumberByName("Hans Maier"));
   }
 }
---
+++ TelephoneBook.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+public class TelephoneBook {
+
+  private HashMap<Person, TelephoneNumber> entries = new HashMap<>();
+
+  public void addEntry(Person person, TelephoneNumber telephoneNumber) {
+    entries.put(person, telephoneNumber);
+  }
+
+  public TelephoneNumber getTelephoneNumberByName(String name) {
+    for (Entry<Person, TelephoneNumber> entry : entries.entrySet()) {
+      if (entry.getKey().name().equals(name)) {
+        return entry.getValue();
+      }
+    }
+    return null;
+  }
+
+  public record Person(String name) {
+  }
+
+  public record TelephoneNumber(String value) {
+  }
+}
