3d352fe86174e6979d285e0c876b3aa5c473eda4
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2023-08-19 13:11:56 +0200

    add lambda syntax examples

--- demo/lambdas/Example.java
+++ demo/lambdas/Example.java
@@ -3,5 +3,28 @@ package demo.lambdas;
 public class Example {
     public static void main(String[] args) {
         ShoppingList sl = new ShoppingList();
+        // Ein Parameter lange Syntax
+        sl.addAfterItemAddedConsumer((item) -> {
+            System.out.println("Item wurde hinzugefügt!");
+        });
+        // Ein Parameter kurze Syntax
+        sl.addAfterItemAddedConsumer(item -> System.out.println("Item wurde hinzugefügt!"));
+
+        // Kein Parameter lange Syntax
+        sl.addAfterListPrintedConsumer(() -> {
+            System.out.println("Liste wurde ausgegeben!");
+        });
+
+        // Kein Parameter kurze Syntax
+        sl.addAfterListPrintedConsumer(() -> System.out.println("Liste wurde ausgegeben!"));
+
+        // Zwei Parameter lange Syntax
+        sl.addItemChangesConsumer((items, changeCount) -> {
+            System.out.println("Items wurde " + changeCount + " mal geändert!");
+        });
+
+        // Zwei Parameter kurze Syntax
+        sl.addItemChangesConsumer((items, changeCount) -> System.out.println("Change nummer: " + changeCount));
+
     }
 }
