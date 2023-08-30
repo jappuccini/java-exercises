3480e04e9558ec0b0a0d410f16618f095135da58
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2023-08-31 00:43:19 +0200

    implement solution

--- Exercise.java
+++ Exercise.java
@@ -4,14 +4,14 @@ public class Exercise {
     Student steffen = new Student("Steffen", 28);
     Student kid = new Student("Kind", 15);
 
-    FilteredAdultStudents adultStudents = new FilteredAdultStudents();
+    FilteredStudents adultStudents = new FilteredStudents((student) -> student.age > 18);
     adultStudents.add(steffen);
     adultStudents.add(kid);
     adultStudents.printStudents();
 
     System.out.println("-----------------------");
 
-    FilteredTeenStudents teenStudents = new FilteredTeenStudents();
+    FilteredStudents teenStudents = new FilteredStudents(student -> student.age < 18);
     teenStudents.add(steffen);
     teenStudents.add(kid);
     teenStudents.printStudents();
