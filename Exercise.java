aaf82116d4c3711e4ad7f15d0c1135fbd3edac1d
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2023-08-31 00:11:48 +0200

    prepare exercise

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,19 @@
 public class Exercise {
 
   public static void main(String[] args) {
-    // implement exercise here
+    Student steffen = new Student("Steffen", 28);
+    Student kid = new Student("Kind", 15);
+
+    FilteredAdultStudents adultStudents = new FilteredAdultStudents();
+    adultStudents.add(steffen);
+    adultStudents.add(kid);
+    adultStudents.printStudents();
+
+    System.out.println("-----------------------");
+
+    FilteredTeenStudents teenStudents = new FilteredTeenStudents();
+    teenStudents.add(steffen);
+    teenStudents.add(kid);
+    teenStudents.printStudents();
   }
 }
