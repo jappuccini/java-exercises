90ce1e97d118dd3a90f5772fdc71e130f513940c
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2023-08-31 23:21:14 +0200

    Implement Solution

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,9 @@
 public class Exercise {
 
   public static void main(String[] args) {
-    // implement exercise here
+    Queries queries = new Queries(DataProvider.getConsoles());
+    System.out.println(queries.getAllConsolesByMaker());
+    // make more queries as you wish
   }
+
 }
