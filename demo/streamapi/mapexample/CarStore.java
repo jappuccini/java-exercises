7e2c15711e42f5f6222b56a4693153d88f6e1c29
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2023-08-31 21:06:32 +0200

    ArrayListToMapSolution

--- demo/streamapi/mapexample/CarStore.java
+++ demo/streamapi/mapexample/CarStore.java
@@ -1,8 +1,29 @@
 package demo.streamapi.mapexample;
 
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class CarStore {
+    private Map<String, Car> carMap;
+    private Function<Car, String> carToKey = car -> car.brand().toLowerCase() + car.model().toLowerCase();
+    private Function<Car, Car> carToValue = car -> car;
+
+    public CarStore() {
+        this.carMap = this.getCarStream()
+                .collect(Collectors.toMap(carToKey, carToValue));
+    }
+
+    public Optional<Car> getCarByKey(String key) {
+        String lookUpKey = key.toLowerCase();
+        if (this.carMap.containsKey(lookUpKey)) {
+            return Optional.of(this.carMap.get(lookUpKey));
+        }
+        return Optional.empty();
+    }
+
     private Stream<Car> getCarStream() {
         return Stream.of(
                 new Car("Fiat", "500", 92),
--- demo/streamapi/mapexample/Example.java
+++ demo/streamapi/mapexample/Example.java
@@ -1,6 +1,11 @@
 package demo.streamapi.mapexample;
 
+import java.util.Optional;
+
 public class Example {
     public static void main(String[] args) {
+        CarStore carStore = new CarStore();
+        Optional<Car> car = carStore.getCarByKey("bmw320i");
+        car.ifPresent(System.out::println);
     }
 }
