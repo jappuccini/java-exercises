ac783e8941b4f26ed125d15b9165ba1b7b0a5b26
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2023-10-16 15:42:43 +0200

    Implement Solution

--- src/main/java/Queue.java
+++ src/main/java/Queue.java
@@ -4,14 +4,49 @@ public class Queue<T> {
     public Node<T> tail;
 
     public Queue() {
+        length = 0;
+        head = null;
+        tail = null;
     }
 
     public void enqueue(T item) {
+        if (item == null) {
+            return;
+        }
+        length++;
+        final Node<T> node = new Node<T>(item, null);
+        if (this.length == 1) {
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            tail = node;
+        }
     }
 
     public T deque() {
+        if (length == 0) {
+            return null;
+        } else if (length == 1) {
+            length--;
+            Node<T> tmp = head;
+            tmp.next = null;
+            head = null;
+            tail = null;
+            return tmp.value;
+        } else {
+            length--;
+            Node<T> tmp = head;
+            head = head.next;
+            tmp.next = null;
+            return tmp.value;
+        }
     }
 
     public T peek() {
+        if (head != null) {
+            return head.value;
+        }
+        return null;
     }
 }
