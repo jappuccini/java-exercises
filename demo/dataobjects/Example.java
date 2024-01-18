593e2b4eb067ff246c01d6702b6db851de0e554d
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2024-01-18 16:05:07 +0100

    add dataobjects example

---
+++ demo/dataobjects/Example.java
@@ -0,0 +1,13 @@
+package demo.dataobjects;
+
+public class Example {
+    public static void main(String[] args) {
+        boolean acceptedAgb = false;
+        acceptedAgb = true;
+        int age = 28;
+        double height = 1.70;
+        char gender = 'm';
+        String myName;
+        myName = "Steffen";
+    }
+}
