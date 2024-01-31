2abf5ef44f88e589a6d090581227ea5b2d5cee9b
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2024-02-01 00:09:42 +0100

    Implement Solution for Arrays 07

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,23 @@
 public class Exercise {
 
   public static void main(String[] args) {
-    // implement exercise here
+    int[] one = { 1, 2, 3, 4 };
+    int[] two = { 5, 6, 7, 8 };
+    int[] both = Exercise.merge(one, two);
+    for (int number : both) {
+      System.out.println(number);
+    }
+  }
+
+  public static int[] merge(int[] first, int[] second) {
+    int newSize = first.length + second.length;
+    int[] mergedArray = new int[newSize];
+    for (int i = 0; i < first.length; i++) {
+      mergedArray[i] = first[i];
+    }
+    for (int i = 0; i < second.length; i++) {
+      mergedArray[i + first.length] = second[i];
+    }
+    return mergedArray;
   }
 }
