2dd284fc78470961d756c1319e3fd4bd8b1ae52b
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2024-02-14 20:07:21 +0100

    Starter

---
+++ Exercise.java
@@ -0,0 +1,12 @@
+public class Exercise {
+
+  public static void main(String[] args) {
+
+    Vehicle vehicle = new Vehicle("Porsche", "911");
+
+    vehicle.accelerate(30);
+    vehicle.accelerate(30);
+    vehicle.brake(20);
+    vehicle.accelerate(40);
+  }
+}
