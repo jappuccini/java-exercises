347ca1cb3258a0f7638c42979e78bc55b6dd1db9
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2024-03-02 10:54:46 +0100

    fix solution

--- CoordinateComparator.java
+++ CoordinateComparator.java
@@ -2,8 +2,13 @@ import java.util.Comparator;
 
 public class CoordinateComparator implements Comparator<Coordinate> {
 
-  @Override
   public int compare(Coordinate c1, Coordinate c2) {
-    return Double.valueOf(c1.getDistanceToOriginPoint()).compareTo(c2.getDistanceToOriginPoint());
+    if (c1.getDistanceToOriginPoint() > c2.getDistanceToOriginPoint()) {
+      return 1;
+    } else if (c1.getDistanceToOriginPoint() < c2.getDistanceToOriginPoint()) {
+      return -1;
+    } else {
+      return 0;
+    }
   }
 }
