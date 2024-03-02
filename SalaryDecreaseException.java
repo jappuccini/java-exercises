7e5d1fc8bf53cb646cd155c9bf2be21acc6d9585
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2024-03-02 12:15:03 +0100

    fix solution

--- SalaryDecreaseException.java
+++ SalaryDecreaseException.java
@@ -1,7 +1,5 @@
 public class SalaryDecreaseException extends Exception {
 
-  private static final long serialVersionUID = 1L;
-
   public SalaryDecreaseException() {
     super("Das neue Gehalt muss hoeher sein als das bisherige");
   }
--- SalaryIncreaseTooHighException.java
+++ SalaryIncreaseTooHighException.java
@@ -1,7 +1,5 @@
 public class SalaryIncreaseTooHighException extends Exception {
 
-  private static final long serialVersionUID = 1L;
-
   public SalaryIncreaseTooHighException() {
     super("Das neue Gehalt darf maximal 10% ueber dem bisherigen Gehalt liegen");
   }
