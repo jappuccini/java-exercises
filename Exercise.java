82c5ee9fe01f7830bb493023f3c285f4ae925864
sgglko <jonas.siggelkow@unitybox.de>
2024-03-03 20:01:44 +0100

    fixed #62 problem with entering date after 24th december.
    there was also a problem with leap years. fixed this too

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,30 @@
 public class Exercise {
 
-  public static void main(String[] args) {
-    // implement exercise here
-  }
+    public static void main(String[] args) {
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Gib bitte ein Datum ein (dd.mm.yyyy): ");
+        String input = sc.next();
+
+        int day = Integer.valueOf(input.substring(0, 2));
+        int month = Integer.valueOf(input.substring(3, 5));
+        int year = Integer.valueOf(input.substring(6, 10));
+
+        LocalDate inputDate = LocalDate.of(year, month, day);
+        LocalDate christmas;
+
+        System.out.println("Wochentag: " + inputDate.getDayOfWeek());
+
+        if (day <= 24 || month < 12) {
+            christmas = LocalDate.of(year, 12, 24);
+            int x = christmas.getDayOfYear() - inputDate.getDayOfYear();
+            System.out.printf("Tage bis Weihnachte: %d%n", x);
+        } else {
+            christmas = LocalDate.of(year + 1, 12, 24);
+            int a = LocalDate.of(year,12,31).getDayOfYear() - inputDate.getDayOfYear();
+            int b = a + christmas.getDayOfYear();
+            System.out.printf("Tage bis Weihnachten: %d%n",b);
+        }
+    }
 }
