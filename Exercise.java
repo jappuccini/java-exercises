9a0f10ffc9b57b67a92cc6c31491cd3c29ee8063
sgglko <jonas.siggelkow@unitybox.de>
2024-03-04 09:03:55 +0100

    fixed #63 deleted static method and implemented solution in main method

--- Exercise.java
+++ Exercise.java
@@ -5,13 +5,6 @@ public class Exercise {
     int age = 40;
     char gender = 'M';
 
-    Exercise.printPersonData("Daniel", 40, 'M');
-    Exercise.printPersonData(name, age, gender);
-  }
-
-  public static void printPersonData(String name, int age, char gender) {
-    System.out.println("Name: " + name);
-    System.out.println("Alter: " + age);
-    System.out.println("Geschlecht: " + gender);
+    System.out.printf("Name: %s; Alter: %d; Geschlecht: %c;",name,age,gender);
   }
 }
