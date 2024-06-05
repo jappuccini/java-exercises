dded37187ac2265991084bcdf0ca3651c4c4fa3c
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2024-06-05 21:59:33 +0200

    fix imports

--- BookCollection.java
+++ BookCollection.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
-import java.util.Optional;
 
 public record BookCollection(HashMap<Author, List<Book>> collection) {
 
