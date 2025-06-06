90dfab344b91f11e965bf0dff98bf2d6f62296bc
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2025-06-06 22:45:32 +0200

    Solution Optionals 03

--- Crate.java
+++ Crate.java
@@ -1,3 +1,5 @@
+import java.util.Optional;
+
 public class Crate<T> {
 
   private T box1;
@@ -22,18 +24,20 @@ public class Crate<T> {
     }
   }
 
-  public T takeBottle(int box) throws CrateIndexOutOfBoundsException {
+  public Optional<T> takeBottle(int box) throws CrateIndexOutOfBoundsException {
     if (box < 1 || box > 6) {
       throw new CrateIndexOutOfBoundsException();
     }
 
-    return switch (box) {
+    T foundBox = switch (box) {
       case 1 -> box1;
       case 2 -> box2;
       case 3 -> box3;
       case 4 -> box4;
       case 5 -> box5;
-      default -> box6;
+      case 6 -> box6;
+      default -> null;
     };
+    return Optional.ofNullable(foundBox);
   }
 }
