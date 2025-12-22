452e3dd47f6495ed807362062725dc70a8a6079e
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2025-12-22 19:52:53 +0100

    Initial Commit

---
+++ Truck.java
@@ -0,0 +1,32 @@
+public final class Truck extends Vehicle {
+
+  private final int cargo;
+  private boolean isTransformed;
+
+  public Truck(String make, String model, Engine engine, int cargo) {
+    super(make, model, engine);
+    this.cargo = cargo;
+  }
+
+  public int getCargo() {
+    return cargo;
+  }
+
+  public boolean isTransformed() {
+    return isTransformed;
+  }
+
+  public void transform() {
+    if (isTransformed) {
+      System.out.println(
+          getMake() + " " + getModel() + " verwandelt sich in einen Lastwagen zurueck");
+    } else {
+      System.out.println(getMake() + " " + getModel() + " verwandelt sich in einen Autobot");
+    }
+    isTransformed = !isTransformed;
+  }
+
+  public String toString() {
+    return getMake() + " " + getModel() + " (" + getEngine().getDescription() + ", " + cargo + "t)";
+  }
+}
