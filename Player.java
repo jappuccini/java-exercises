aec0992a392b9764d723df4ba7d8f65105e34ced
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2025-12-22 18:41:00 +0100

    Initial Commit

---
+++ Player.java
@@ -0,0 +1,21 @@
+public class Player {
+
+  private String name;
+  private int score;
+
+  public Player(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getScore() {
+    return score;
+  }
+
+  public void setScore(int score) {
+    this.score = score;
+  }
+}
