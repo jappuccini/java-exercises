2bf20e97ad35a214ac3f7643c9206de3f612d530
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2025-12-22 15:24:56 +0100

    Solution

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,10 @@
 public class Exercise {
 
   public static void main(String[] args) {
-    // implement exercise here
+    for (int i = 1; i <= 10; i++) {
+      for (int j = 1; j <= 10; j++) {
+        System.out.println(i + " x " + j + " = " + (i * j));
+      }
+    }
   }
 }
