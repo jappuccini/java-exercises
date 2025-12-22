e4975fc5d69d05a596fb77ddff4f7acb47f4f975
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2025-12-22 15:52:49 +0100

    Initial Commit

---
+++ Vehicle.java
@@ -0,0 +1,37 @@
+public class Vehicle {
+
+  private String make;
+  private String model;
+  private double speedInKmh;
+
+  public Vehicle(String make, String model) {
+    this.make = make;
+    this.model = model;
+  }
+
+  public String getMake() {
+    return make;
+  }
+
+  public String getModel() {
+    return model;
+  }
+
+  public double getSpeedInKmh() {
+    return speedInKmh;
+  }
+
+  public void accelerate(int valueInKmh) {
+    speedInKmh += valueInKmh;
+    System.out.println(toString() + " beschleunigt auf " + speedInKmh + "km/h");
+  }
+
+  public void brake(int valueInKmh) {
+    speedInKmh -= valueInKmh;
+    System.out.println(toString() + " bremst auf " + speedInKmh + "km/h ab");
+  }
+
+  public String toString() {
+    return make + " " + model;
+  }
+}
