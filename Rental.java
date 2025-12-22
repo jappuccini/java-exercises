28d835309ffecf879bc318a0de876544bc893798
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2025-12-22 19:50:51 +0100

    Initial Commit

---
+++ Rental.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+public class Rental {
+
+  private final String name;
+  private final ArrayList<Vehicle> vehicles;
+
+  public Rental(String name) {
+    this.name = name;
+    vehicles = new ArrayList<>();
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public ArrayList<Vehicle> getVehicles() {
+    return vehicles;
+  }
+
+  public void addVehicle(Vehicle vehicle) {
+    vehicles.add(vehicle);
+  }
+
+  public void addAllVehicles(Vehicle... vehicles) {
+    for (Vehicle v : vehicles) {
+      this.vehicles.add(v);
+    }
+  }
+
+  public void transformAllTrucks() {
+    for (Vehicle c : vehicles) {
+      if (c instanceof Truck) {
+        Truck t = (Truck) c;
+        t.transform();
+      }
+    }
+  }
+
+  public String toString() {
+    String result = name + "\n" + "Unsere Fahrzeuge: \n";
+    for (Vehicle vehicle : vehicles) {
+      result += vehicle.toString() + "\n";
+    }
+    return result;
+  }
+}
