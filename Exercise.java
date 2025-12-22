a8c4a35baaad83cfb158360affd82ca1788d92e3
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2025-12-22 19:34:53 +0100

    Initial Commit

---
+++ Exercise.java
@@ -0,0 +1,12 @@
+public class Exercise {
+
+  public static void main(String[] args) {
+    System.out.println("Anzahl Fahrzeuge: " + Vehicle.getNumberOfVehicles());
+
+    Vehicle vehicle1 = new Vehicle("Porsche", "911", Engine.ELECTRO);
+    Vehicle vehicle2 = new Vehicle("MAN", "TGX", Engine.DIESEL);
+    Vehicle vehicle3 = new Vehicle("Opel", "Zafira Life", Engine.DIESEL);
+
+    System.out.println("Anzahl Fahrzeuge: " + Vehicle.getNumberOfVehicles());
+  }
+}
---
+++ Vehicle.java
@@ -0,0 +1,49 @@
+public class Vehicle {
+
+  private String make;
+  private String model;
+  private double speedInKmh;
+  private Engine engine;
+  private static int numberOfVehicles;
+
+  public Vehicle(String make, String model, Engine engine) {
+    this.make = make;
+    this.model = model;
+    Vehicle.numberOfVehicles++;
+    this.engine = engine;
+  }
+
+  public String getMake() {
+    return make;
+  }
+
+  public String getModel() {
+    return model;
+  }
+
+  public Engine getEngine() {
+    return engine;
+  }
+
+  public double getSpeedInKmh() {
+    return speedInKmh;
+  }
+
+  public void accelerate(int valueInKmh) {
+    speedInKmh += valueInKmh;
+    System.out.println(toString() + " beschleunigt auf " + speedInKmh + "km/h");
+  }
+
+  public void brake(int valueInKmh) {
+    speedInKmh -= valueInKmh;
+    System.out.println(toString() + " bremst auf " + speedInKmh + "km/h ab");
+  }
+
+  public String toString() {
+    return make + " " + model;
+  }
+
+  public static int getNumberOfVehicles() {
+    return Vehicle.numberOfVehicles;
+  }
+}
