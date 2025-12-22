40d295fba8aab4a0d541485f72ae4eabe0c08a35
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2025-12-22 19:59:44 +0100

    Solution

--- Exercise.java
+++ Exercise.java
@@ -13,14 +13,14 @@ public class Exercise {
     coordinates.add(new Coordinate(6, 8));
     coordinates.add(new Coordinate(1, 9));
 
-    class CoordinateComparator implements Comparator<Coordinate> {
-      @Override
-      public int compare(Coordinate c1, Coordinate c2) {
-        return Integer.valueOf(c1.getX()).compareTo(c2.getX());
-      }
-    }
-
-    Collections.sort(coordinates, new CoordinateComparator());
+    Collections.sort(
+        coordinates,
+        new Comparator<Coordinate>() {
+          @Override
+          public int compare(Coordinate c1, Coordinate c2) {
+            return Integer.valueOf(c1.getY()).compareTo(c2.getY());
+          }
+        });
 
     for (Coordinate c : coordinates) {
       System.out.println(c + ": " + c.getDistanceToOriginPoint());
