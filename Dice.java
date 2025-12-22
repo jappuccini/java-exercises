04a88622a1f15f8c9aa1b90cc99fa031d48b7718
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2025-12-22 15:52:21 +0100

    Initial Commit

---
+++ Dice.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class Dice {
+
+  private int id;
+  private int value;
+
+  public Dice(int id) {
+    this.id = id;
+    rollTheDice();
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public int getValue() {
+    return value;
+  }
+
+  public void rollTheDice() {
+    Random random = new Random();
+    value = random.nextInt(6) + 1;
+  }
+}
