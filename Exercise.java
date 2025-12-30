bc96f3bced8af331dc6bf8296f8e5eacfa626cd2
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2025-12-30 12:15:00 +0100

    Initial Commit

---
+++ Exercise.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Exercise {
+
+  public static void main(String[] args) {
+    ArrayList<Student> students = new ArrayList<>();
+    students.add(new Student("Steffen", 28));
+    students.add(new Student("Marianna", 24));
+    students.add(new Student("Mirco", 26));
+    students.add(new Student("Dennis", 30));
+    students.add(new Student("Peter", 29));
+    students.add(new Student("Leander", 29));
+    students.add(new Student("Alu", 32));
+    students.add(new Student("Yannik", 28));
+    students.add(new Student("Hanni", 29));
+    students.add(new Student("Manu", 30));
+  }
+}
---
+++ Student.java
@@ -0,0 +1,1 @@
+public record Student(String name, int age) {}
