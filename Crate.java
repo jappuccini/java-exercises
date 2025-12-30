ea70b10742c41c6379e530aa831f4ae6a546d6cf
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2025-12-30 12:22:54 +0100

    Solution

--- Crate.java
+++ Crate.java
@@ -1,13 +1,13 @@
-public class Crate {
+public class Crate<T> {
 
-  private Bottle box1;
-  private Bottle box2;
-  private Bottle box3;
-  private Bottle box4;
-  private Bottle box5;
-  private Bottle box6;
+  private T box1;
+  private T box2;
+  private T box3;
+  private T box4;
+  private T box5;
+  private T box6;
 
-  public void insertBottle(Bottle bottle, int box) throws CrateIndexOutOfBoundsException {
+  public void insertBottle(T bottle, int box) throws CrateIndexOutOfBoundsException {
     if (box < 1 || box > 6) {
       throw new CrateIndexOutOfBoundsException();
     }
@@ -22,7 +22,7 @@ public class Crate {
     }
   }
 
-  public Bottle takeBottle(int box) throws CrateIndexOutOfBoundsException {
+  public T takeBottle(int box) throws CrateIndexOutOfBoundsException {
     if (box < 1 || box > 6) {
       throw new CrateIndexOutOfBoundsException();
     }
