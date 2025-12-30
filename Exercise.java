0984573ef75a184e1fbc02c6f1be70ba06444b6b
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2025-12-30 11:43:22 +0100

    Solution

--- Exercise.java
+++ Exercise.java
@@ -3,10 +3,13 @@ public class Exercise {
   public static void main(String[] args) {
     System.out.println("Anzahl Fahrzeuge: " + Vehicle.getNumberOfVehicles());
 
-    Vehicle vehicle = new Vehicle("Porsche", "911");
-    Vehicle vehicle1 = new Vehicle("Porsche", "911");
-    Vehicle vehicle2 = new Vehicle("MAN", "TGX");
-    Vehicle vehicle3 = new Vehicle("Opel", "Zafira Life");
+    Vehicle vehicle1 = new Vehicle("Porsche", "911", Engine.ELECTRO);
+    Vehicle vehicle2 = new Vehicle("MAN", "TGX", Engine.DIESEL);
+    Vehicle vehicle3 = new Vehicle("Opel", "Zafira Life", Engine.DIESEL);
+
+    System.out.println(vehicle1.toString());
+    System.out.println(vehicle2.toString());
+    System.out.println(vehicle3.toString());
 
     System.out.println("Anzahl Fahrzeuge: " + Vehicle.getNumberOfVehicles());
   }
