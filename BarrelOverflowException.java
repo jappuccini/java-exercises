22f4ef7a3790930490aea16ad065cb7ed1f8d8d4
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2025-12-30 12:10:08 +0100

    Solution

---
+++ BarrelOverflowException.java
@@ -0,0 +1,6 @@
+public class BarrelOverflowException extends Exception {
+
+  public BarrelOverflowException() {
+    super("Das war der Tropfen, der das Fass zum Ueberlaufen gebracht hat");
+  }
+}
