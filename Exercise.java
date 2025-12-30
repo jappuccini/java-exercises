d9e6b2b60c834c8ded77f6e74e1f5e87495523b3
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2025-12-30 12:28:17 +0100

    Solution

--- Exercise.java
+++ Exercise.java
@@ -11,9 +11,15 @@ public class Exercise {
       crate.insertBottle(new WineBottle(), 5);
       crate.insertBottle(new WineBottle(), 6);
 
-      if (crate.takeBottle(3) instanceof BeerBottle beerBottle) {
-        beerBottle.chugALug();
-      }
+      crate
+          .takeBottle(3)
+          .ifPresentOrElse(
+              bottle -> {
+                if (bottle instanceof BeerBottle beerBottle) {
+                  beerBottle.chugALug();
+                }
+              },
+              () -> System.out.println("Gesuchte Flasche ist nicht vorhanden."));
     } catch (CrateIndexOutOfBoundsException e) {
       System.err.println(e.getMessage());
     }
