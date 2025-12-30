53f69bce744099cac03ecb876deb99c271536564
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2025-12-30 12:23:03 +0100

    Solution

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,22 @@
+import java.util.ArrayList;
+
 public class Exercise {
 
   public static void main(String[] args) {
-    // implement exercise here
+
+    SchoolClass schoolClass = new SchoolClass("9c", new ArrayList<>());
+
+    schoolClass.addPupil(new Pupil("Hans", "m"));
+    schoolClass.addPupil(new Pupil("Peter", "m"));
+    schoolClass.addPupil(new Pupil("Lisa", "w"));
+    schoolClass.addPupil(new Pupil("Fritz", "m"));
+    schoolClass.addPupil(new Pupil("Max", "m"));
+    schoolClass.addPupil(new Pupil("Franziska", "w"));
+    schoolClass.addPupil(new Pupil("Jennifer", "w"));
+    // schoolClass.addPupil(new Pupil("Jeremy", "m"));
+
+    for (Pair<Pupil> pair : schoolClass.getPairs()) {
+      System.out.println(pair);
+    }
   }
 }
---
+++ Pair.java
@@ -0,0 +1,1 @@
+public record Pair<T>(T partA, T partB) {}
