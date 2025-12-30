26092ff10f1b2fdda6387e728d9e9cda3e311edb
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2025-12-30 12:23:12 +0100

    Solution

---
+++ Tournament.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+public record Tournament(String title, List<Club> clubs, List<Pair<Club>> pairs) {
+
+  public void addClub(Club club) {
+    clubs.add(club);
+  }
+
+  public List<Pair<Club>> pairs() {
+    for (int i = 0; i < clubs.size(); i++) {
+      for (int j = 0; j < clubs.size(); j++) {
+        if (i != j) {
+          pairs.add(new Pair<Club>(clubs.get(i), clubs.get(j)));
+        }
+      }
+    }
+
+    return pairs;
+  }
+}
