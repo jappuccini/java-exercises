cad77abe16a8e3350c1d942a6796638c701123ba
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2025-12-30 12:11:45 +0100

    Solution

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,14 @@
 public class Exercise {
 
   public static void main(String[] args) {
-    // implement exercise here
+    House house = new House();
+
+    house.addRoom(house.new Room("Wohnzimmer"));
+    house.addRoom(house.new Room("Esszimmer"));
+    house.addRoom(house.new Room("Schlafzimmer"));
+    house.addRoom(house.new Room("Kueche"));
+    house.addRoom(house.new Room("WC"));
+
+    house.displayRooms();
   }
 }
